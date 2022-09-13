46cb6f48e77a0341421e82fc639be40c0776e798
Adrien Calmesnil <adcalmesnil@gmail.com>
2022-09-13 22:35:54 +0200

    Health bar + new monsters

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
+let monstersArray = ["orc", "demon", "goblin"];
+
+/*
+Challenge
+1. Create a function called getNewMonster.
+2. Write logic inside the function that takes the first 
+monster from monstersArray and extracts that monster's 
+data from characterData.
+3. Save that data to a new const called nextMonsterData.
+**hint.md for help!!**
+*/
+
 function attack() {
     wizard.getDiceHtml()
     orc.getDiceHtml()
+    wizard.takeDamage(orc.currentDiceScore)
+    orc.takeDamage(wizard.currentDiceScore)
     render()
+    
+        if(wizard.dead || orc.dead){
+            endGame()
+        }      
 }
 
+function endGame() {
+    const endMessage = wizard.health === 0 && orc.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The Orc is Victorious"
 
-function render() {
-    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    document.body.innerHTML = `
+        <div class="end-game">
+            <h2>Game Over</h2> 
+            <h3>${endMessage}</h3>
+            <p class="end-emoji">${endEmoji}</p>
+        </div>
+        `
 }
 
 document.getElementById("attack-button").addEventListener('click', attack)
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+}
 
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
